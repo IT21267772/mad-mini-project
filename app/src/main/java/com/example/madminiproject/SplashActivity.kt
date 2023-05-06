c0fd2eb3bdbc9117128fa25636c98448a3f115bb
Prathibha <it21170584@my.sliit.lk>
2023-05-06 08:01:06 +0530

    new commit

--- app/src/main/java/com/example/madminiproject/SplashActivity.kt
+++ app/src/main/java/com/example/madminiproject/SplashActivity.kt
@@ -3,16 +3,27 @@ package com.example.madminiproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var nextScreen: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Display the splash screen layout
         setContentView(R.layout.activity_splash)
 
+        nextScreen = findViewById(R.id.button)
+
+        nextScreen.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         // Launch the main activity after a delay
-        val splashTimer = object : Thread() {
+        /*val splashTimer = object : Thread() {
             override fun run() {
                 try {
                     sleep(2000) // 2 seconds delay
@@ -24,10 +35,13 @@ class SplashActivity : AppCompatActivity() {
             }
         }
         splashTimer.start()
-    }
+    }*/
 
-    override fun onPause() {
-        super.onPause()
-        finish() // Finish the splash screen activity to prevent going back to it
+
+
+        /*override fun onPause() {
+            super.onPause()
+            finish() // Finish the splash screen activity to prevent going back to it
+        }*/
     }
 }
